4285b2bdd1831480113cf4b73a86bf6ab3819a90
Hollin Ariyawansa <ariyawansahollin@gmail.com>
2026-03-11 00:24:43 +1100

    feat: add API endpoint for creating orders

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { CreateOrderDto } from './dto/create-order.dto';
 
 @Controller('orders')
-export class OrdersController {}
+export class OrdersController {
+  constructor(private orderService: OrdersService) {}
+
+  @Post()
+  create(@Body() dto: CreateOrderDto) {
+    return this.orderService.createOrder(dto);
+  }
+}
