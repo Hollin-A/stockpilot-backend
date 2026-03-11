28654bf54b1aac474a387a2251c2e67ec59401b9
Hollin Ariyawansa <ariyawansahollin@gmail.com>
2026-03-11 23:37:47 +1100

    feat: create purchase orders module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
 import { SuppliersModule } from './suppliers/suppliers.module';
+import { PurchaseOrdersModule } from './purchase-orders/purchase-orders.module';
 
 @Module({
-  imports: [PrismaModule, ProductsModule, OrdersModule, SuppliersModule],
+  imports: [PrismaModule, ProductsModule, OrdersModule, SuppliersModule, PurchaseOrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
