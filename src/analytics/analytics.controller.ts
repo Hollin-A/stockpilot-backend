88bed2dac932dfbb6161cda4b32d822b02b50988
Hollin Ariyawansa <ariyawansahollin@gmail.com>
2026-03-12 00:34:42 +1100

    feat: expose analytics API endpoints

--- src/analytics/analytics.controller.ts
+++ src/analytics/analytics.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { AnalyticsService } from './analytics.service';
 
 @Controller('analytics')
-export class AnalyticsController {}
+export class AnalyticsController {
+  constructor(private analyticsService: AnalyticsService) {}
+
+  @Get('sales')
+  getSalesSummary() {
+    return this.analyticsService.getSalesSummary();
+  }
+
+  @Get('top-products')
+  getTopProducts() {
+    return this.analyticsService.getTopProducts();
+  }
+
+  @Get('low-stock')
+  getLowStockProducts() {
+    return this.analyticsService.getLowStockProducts();
+  }
+
+  @Get('stock-movements')
+  getStockMovements(@Query('productId') productId?: string) {
+    return this.analyticsService.getStockMovements(productId);
+  }
+}
