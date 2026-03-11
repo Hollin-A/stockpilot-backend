8ecbc9e5db892c0a385065f7aa656df25c6ab490
Hollin Ariyawansa <ariyawansahollin@gmail.com>
2026-03-11 23:20:22 +1100

    feat: implement supplier CRUD endpoints

--- src/suppliers/suppliers.controller.ts
+++ src/suppliers/suppliers.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { SuppliersService } from './suppliers.service';
 
 @Controller('suppliers')
-export class SuppliersController {}
+export class SuppliersController {
+  constructor(private suppliersService: SuppliersService) {}
+
+  @Post()
+  create(@Body() body: any) {
+    return this.suppliersService.create(body);
+  }
+
+  @Get()
+  findAll() {
+    return this.suppliersService.findAll();
+  }
+}
--- src/suppliers/suppliers.service.ts
+++ src/suppliers/suppliers.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class SuppliersService {}
+export class SuppliersService {
+  constructor(private prisma: PrismaService) {}
+
+  create(data: any) {
+    return this.prisma.supplier.create({ data });
+  }
+
+  findAll() {
+    return this.prisma.supplier.findMany();
+  }
+}
