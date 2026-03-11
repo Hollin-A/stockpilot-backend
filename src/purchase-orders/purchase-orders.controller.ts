14adea08765ff5bd94e8bd1d301da69585c4b2f0
Hollin Ariyawansa <ariyawansahollin@gmail.com>
2026-03-11 23:48:39 +1100

    feat: add endpoints for purchase order creation and receiving

--- src/purchase-orders/purchase-orders.controller.ts
+++ src/purchase-orders/purchase-orders.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
+import { PurchaseOrdersService } from './purchase-orders.service';
 
 @Controller('purchase-orders')
-export class PurchaseOrdersController {}
+export class PurchaseOrdersController {
+  constructor(private service: PurchaseOrdersService) {}
+
+  @Post()
+  create(@Body() body: any) {
+    return this.service.createPurchaseOrder(body);
+  }
+
+  @Post(':id/receive')
+  receive(@Param('id') id: string) {
+    return this.service.receivePurchaseOrder(id);
+  }
+}
