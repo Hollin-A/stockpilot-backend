b9068ae83300bfa03857a9a334a9b00c85dc416b
Hollin Ariyawansa <ariyawansahollin@gmail.com>
2026-03-15 02:31:21 +1100

    feat: add CORS, global exception filter, and Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 
 async function bootstrap() {
   if (!process.env.JWT_SECRET) {
     throw new Error('JWT_SECRET environment variable is not set');
   }
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+  app.useGlobalFilters(new GlobalExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('StockPilot API')
+    .setDescription('Inventory management API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  SwaggerModule.setup('api', app, SwaggerModule.createDocument(app, config));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
