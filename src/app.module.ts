15130c3d381a2b08da42e6bedda4acd783755e79
Hollin Ariyawansa <ariyawansahollin@gmail.com>
2026-03-14 11:33:11 +1100

    feat: create auth and users modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,11 @@ import { OrdersModule } from './modules/orders/orders.module';
 import { SuppliersModule } from './modules/suppliers/suppliers.module';
 import { PurchaseOrdersModule } from './modules/purchase-orders/purchase-orders.module';
 import { AnalyticsModule } from './modules/analytics/analytics.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [PrismaModule, ProductsModule, OrdersModule, SuppliersModule, PurchaseOrdersModule, AnalyticsModule],
+  imports: [PrismaModule, ProductsModule, OrdersModule, SuppliersModule, PurchaseOrdersModule, AnalyticsModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
