f4ddea4d4d08cf5c0041bde3361ce12a1b232585
Hollin Ariyawansa <ariyawansahollin@gmail.com>
2026-03-14 12:31:36 +1100

    fix: fix on module imports and exports

--- src/modules/orders/orders.service.ts
+++ src/modules/orders/orders.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../database/prisma/prisma.service';
+import { PrismaService } from '../../database/prisma/prisma.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { MovementType } from '@prisma/client';
 
