ea849bc307dba4c500e851d61b77c4c0a46207c9
Hollin Ariyawansa <ariyawansahollin@gmail.com>
2026-03-22 21:22:19 +1100

    feat: update on cors config

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000',
-      // 'https://your-frontend.vercel.app'
+      'https://stockpilot-lk.vercel.app'
     ],
   });
   app.useGlobalPipes(
